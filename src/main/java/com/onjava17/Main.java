5278ec959ba0b2011ea64a2e310bdc894629cb56
tianyi.zhang <tianyi.zhang@linkflowtech.com>
2023-04-27 16:17:45 +0800

    Description:<feat> main test

--- src/main/java/com/onjava17/Main.java
+++ src/main/java/com/onjava17/Main.java
@@ -1,9 +1,20 @@
 package com.onjava17;
 
 /**
-* @author zhangtian1
-*/public class Main {
+ * @author zhangtian1
+ */
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var name = returnString();
+        switch (name) {
+            case "a" -> System.out.println("this is a");
+            case "b" -> System.out.println("this is b");
+            default -> System.out.println("this is default");
+        }
+
+    }
+
+    public static String returnString() {
+        return "a";
     }
 }
